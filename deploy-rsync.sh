2ac158b9f15d0c6279990327cd67e596a0f53106
Rob Humar <foxgroup@Foxs-iMac.local>
2019-11-20 11:33:47 +0000

    Added rsync

--- deploy-rsync.sh
+++ deploy-rsync.sh
@@ -11,19 +11,22 @@ set -e
 # Set repo based on current branch, by default master=production, develop=staging
 # @todo support custom branches
 
-  if [ "$CI_BRANCH" == "master" ]
-  then
-      target_install=${PROD_INSTALL}
-  else
-      target_install=${DEV_INSTALL}
-  fi
+if [ "$CI_BRANCH" == "master" ]
+then
+    target_install=${PROD_INSTALL_IP}
+else
+    target_install=${DEV_INSTALL_IP}
+fi
 
 # Install plugin/theme packages
-  composer install
+composer install
 
 # Install theme packages and compile into production version
-  yarn cache clean && yarn && yarn run build:production
+yarn cache clean && yarn && yarn run build:production
 
 # Rsync to directory on server
-  echo "Syncing theme to server: ${SSH_USERNAME}@${target_install}:~/public_html/wp-content/themes/${REPO_NAME}"
-  rsync -avz -e "ssh" ~/clone/ ${SSH_USERNAME}@${target_install}:~/public_html/wp-content/themes/${REPO_NAME}
+# Create theme directory if not exist
+ssh ${SSH_USERNAME}@${target_install} mkdir -p ~/public_html/wp-content/themes/${REPO_NAME}
+
+echo "Syncing theme to server: ${SSH_USERNAME}@${target_install}:~/public_html/wp-content/themes/${REPO_NAME}"
+rsync -avz -e "ssh" ~/clone/ ${SSH_USERNAME}@${target_install}:~/public_html/wp-content/themes/${REPO_NAME}
